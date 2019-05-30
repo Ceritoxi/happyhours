ef1b528880ec7c00d58231b69ff5ec7e36bad058
Zoltan Levay <zoltan_levay@epam.com>
2019-05-30 11:33:45 +0200

    made bar abstract duh

--- HappyJava/src/main/java/com/zlotran/happyhours/ui/bar/ColorChangerRefreshableBar.java
+++ HappyJava/src/main/java/com/zlotran/happyhours/ui/bar/ColorChangerRefreshableBar.java
@@ -3,7 +3,7 @@ package com.zlotran.happyhours.ui.bar;
 import com.zlotran.happyhours.ui.refresher.LabelRefresher;
 import com.zlotran.happyhours.ui.util.ColorCalculatorUtil;
 
-public class ColorChangerRefreshableBar extends RefreshableBar {
+public abstract class ColorChangerRefreshableBar extends RefreshableBar {
 
     private static final int PEAK_YELLOW = 28800;
 

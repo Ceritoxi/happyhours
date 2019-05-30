9407d8c37976fab312b21e41b3c869abb6d95965
Zoltan Levay <zoltan_levay@epam.com>
2019-05-30 11:20:26 +0200

    color thing refactor

--- HappyJava/src/main/java/com/zlotran/happyhours/ui/bar/ColorChangerRefreshableBar.java
+++ HappyJava/src/main/java/com/zlotran/happyhours/ui/bar/ColorChangerRefreshableBar.java
@@ -3,10 +3,11 @@ package com.zlotran.happyhours.ui.bar;
 import java.awt.Color;
 
 import com.zlotran.happyhours.ui.refresher.LabelRefresher;
+import com.zlotran.happyhours.ui.util.ColorCalculatorUtil;
 
 public class ColorChangerRefreshableBar extends RefreshableBar {
 
-    private static final double PEAK_YELLOW = 27000d;
+    private static final int PEAK_YELLOW = 27000;
 
     ColorChangerRefreshableBar(LabelRefresher labelRefresher) {
         super(labelRefresher);
@@ -15,18 +16,8 @@ public class ColorChangerRefreshableBar extends RefreshableBar {
     @Override
     public void refresh() {
         super.refresh();
-        this.setForeground(calcColor());
+        this.setForeground(ColorCalculatorUtil.calcColor(barProgress, PEAK_YELLOW, FROM, TO));
     }
 
-    private Color calcColor() {
-        Color color;
-        if (barProgress < PEAK_YELLOW) {
-            color = new Color(255, (int) (((barProgress - FROM) / (PEAK_YELLOW - FROM)) * (255 - 50)), 0);
-        } else if (barProgress <= TO) {
-            color = new Color(255 - (int) (((barProgress - PEAK_YELLOW) / (TO - PEAK_YELLOW)) * 255), 255 - 50, 0);
-        } else {
-            color = new Color(0, 255 - 50, 0);
-        }
-        return color;
-    }
+
 }
---
+++ HappyJava/src/main/java/com/zlotran/happyhours/ui/util/ColorCalculatorUtil.java
@@ -0,0 +1,18 @@
+package com.zlotran.happyhours.ui.util;
+
+import java.awt.Color;
+
+public class ColorCalculatorUtil {
+
+    public static Color calcColor(int barProgress, int peakYellow, int minBarProgress, int maxBarProgress) {
+        Color color;
+        if (barProgress < peakYellow) {
+            color = new Color(255, (int) (((barProgress - minBarProgress) / ((double)peakYellow - minBarProgress)) * (255 - 50)), 0);
+        } else if (barProgress <= maxBarProgress) {
+            color = new Color(255 - (int) (((barProgress - (double)peakYellow) / (maxBarProgress - (double)peakYellow)) * 255), 255 - 50, 0);
+        } else {
+            color = new Color(0, 255 - 50, 0);
+        }
+        return color;
+    }
+}
